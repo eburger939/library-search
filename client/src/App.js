14181f42c0666777c83f916a462fa203f33b5d12
Emily Burger <eburger939@gmail.com>
2022-02-22 19:56:35 -0600

    added apollo/client to app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,36 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
 
-// const client = new ApolloClient({
-//   uri: '/graphql',
-//   cache: new InMemoryCache()
-// })
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache()
+})
 
 function App() {
   return (
+    <ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
         <Switch>
-          <Route exact path='/'>
-            <SearchBooks/>
-            </Route> 
-          <Route exact path='/saved'>
-            <SavedBooks/>
-            </Route>
+          <Route 
+            path="/"
+            element={<SearchBooks/>}
+            /> 
+           <Route 
+            path="/saved"
+            element={<SavedBooks/>}
+            /> 
           <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
         </Switch>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 
